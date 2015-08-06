7313dd2598b6b12c0b4baf450f458f446da4c5a4
onocmouse <onocmouse@gmail.com>
2015-08-06 16:19:04 -0500

    Lots of compression

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,27 +1,54 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.configure do
+	# Version of your assets, change this if you want to expire all your assets.
+	config.assets.version = '1.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+	# Add additional assets to the asset load path
+	# config.assets.paths << Emoji.images_path
 
-Rails.application.config.assets.css_compressor = :sass
+	# Precompile additional assets.
+	# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+	# config.assets.precompile += %w( search.js )
 
-if Rails.env == "development"
-	Rails.application.config.assets.precompile += Rails.application.config.assets.paths.map do |path|
-		Dir.glob("#{path}/**/*.js").map do |file|
-			file.gsub(path+"/","")
-		end +
-		Dir.glob("#{path}/**/*.js.*").map do |file|
-			file.gsub(path+"/","").gsub(/\.js\..*$/,'.js')
-		end
+	config.assets.css_compressor = :sass
+    config.middleware.use Rack::Deflater
+    config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
+
+    config.middleware.use HtmlCompressor::Rack,
+      compress_css: true,
+      compress_javascript: true,
+      css_compressor: Sass,
+      enabled: true,
+      javascript_compressor: uglifier,
+      preserve_line_breaks: false,
+      remove_comments: true,
+      remove_form_attributes: false,
+      remove_http_protocol: false,
+      remove_https_protocol: false,
+      remove_input_attributes: true,
+      remove_intertag_spaces: false,
+      remove_javascript_protocol: true,
+      remove_link_attributes: true,
+      remove_multi_spaces: true,
+      remove_quotes: true,
+      remove_script_attributes: true,
+      remove_style_attributes: true,
+      simple_boolean_attributes: true,
+      simple_doctype: false
+
+	if Rails.env == "development"
+		config.assets.precompile += config.assets.paths.map do |path|
+			Dir.glob("#{path}/**/*.js").map do |file|
+				file.gsub(path+"/","")
+			end +
+			Dir.glob("#{path}/**/*.js.*").map do |file|
+				file.gsub(path+"/","").gsub(/\.js\..*$/,'.js')
+			end
 		
-	end.flatten
-else
-	Rails.application.config.assets.precompile += %w( polyfill.js )
+		end.flatten
+	else
+		config.assets.precompile += %w( polyfill.js )
+	end
 end
