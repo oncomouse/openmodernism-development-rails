b5b32a0b1c9a99c8380cf3cd780daa0c726bbc4c
onocmouse <onocmouse@gmail.com>
2015-08-06 10:40:00 -0500

    Stupid requirement to build CSS files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.css_compressor = :sass
+
 if Rails.env == "development"
 	Rails.application.config.assets.precompile += Rails.application.config.assets.paths.map do |path|
 		Dir.glob("#{path}/**/*.js").map do |file|
