891aec034685691e6629aad34bcce41d259d897d
onocmouse <onocmouse@gmail.com>
2015-08-07 11:50:23 -0500

    Stupid plurals

---
+++ app/controllers/anthologies_controller.rb
@@ -0,0 +1,9 @@
+class AnthologiesController < ApplicationController
+	def index
+		render json: Anthology.preload(:documents).to_json(:include => [:documents])
+	end
+	
+	def view
+		render json: Anthology.preload(:documents).find(params['id']).to_json(:include => [:documents])
+	end
+end
