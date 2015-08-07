00bdc36c7278428f5605f44206017acbf031a7c1
onocmouse <onocmouse@gmail.com>
2015-08-07 11:25:06 -0500

    Correctly generate JSON (and boy was it a pain)

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,9 +1,9 @@
 class DocumentsController < ApplicationController
 	def index
-		render json: Document.all
+		render json: Document.preload(:authors, :citations, :document_files).to_json(:include => [:authors, :citations, :document_files])
 	end
 	
 	def view
-		render json: Document.find_by(id: params['id'])
+		render json: Document.preload(:authors, :citations, :document_files).find(params['id']).to_json(:include => [:authors, :citations, :document_files])
 	end
 end
