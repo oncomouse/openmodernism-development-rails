8c2b9b69df48c6ad91e9707ee4b5dbf94385df99
onocmouse <onocmouse@gmail.com>
2015-08-07 11:42:41 -0500

    Before save to create non-existent authors found in metadata as Author objects.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,10 +1,17 @@
 class Document < ActiveRecord::Base
-	#property :id, Serial
-	#property :metadata, Text, :lazy => false
-	
 	has_and_belongs_to_many :authors, join_table: :documents_authors
 	has_many :citations, dependent: :destroy, autosave: true
 	has_many :document_files, dependent: :destroy, autosave: true
+	
+	def before_save(record)
+		YAML.load(d.metadata)['author'].each do |author|
+			if nil? Author.find_by(given: record['given'], family: record['family'])
+				aa = Author.new
+				aa.from_json(JSON.generate(author))
+				record.authors.push(aa)
+			end
+		end
+	end
 	
 	accepts_nested_attributes_for :citations
 	accepts_nested_attributes_for :authors
