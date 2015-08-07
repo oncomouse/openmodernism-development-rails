a381685fb1dabd11f1cdedc11f7fcf38de13a133
onocmouse <onocmouse@gmail.com>
2015-08-07 11:24:47 -0500

    I'm told this is important?

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,10 +3,10 @@ class Document < ActiveRecord::Base
 	#property :metadata, Text, :lazy => false
 	
 	has_and_belongs_to_many :authors, join_table: :documents_authors
-	has_many :citations
-	has_many :document_files
+	has_many :citations, dependent: :destroy, autosave: true
+	has_many :document_files, dependent: :destroy, autosave: true
 	
-	#has n, :document_files
-	#has_many :citations, :dependent => :destroy
-	#has_many :document_files, :dependent => :destroy
+	accepts_nested_attributes_for :citations
+	accepts_nested_attributes_for :authors
+	accepts_nested_attributes_for :document_files
 end
