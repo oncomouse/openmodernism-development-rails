9b0664b0f552a5474b5610e95a4a8884017761ce
onocmouse <onocmouse@gmail.com>
2015-08-07 11:50:28 -0500

    UGH

--- app/controllers/anthology_controller.rb
+++
@@ -1,9 +0,0 @@
-class AnthologyController < ApplicationController
-	def index
-		render json: Anthology.preload(:documents).to_json(:include => [:documents])
-	end
-	
-	def view
-		render json: Anthology.preload(:documents).find(params['id']).to_json(:include => [:documents])
-	end
-end
