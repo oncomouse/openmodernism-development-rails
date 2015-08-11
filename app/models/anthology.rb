23dbb48ad9e7d1f6c8b7b832a6fd66b14cf06a8a
onocmouse <onocmouse@gmail.com>
2015-08-11 09:34:18 -0500

    Grab documents for an anthology

--- app/models/anthology.rb
+++ app/models/anthology.rb
@@ -1,4 +1,10 @@
 class Anthology < ActiveRecord::Base
 	has_and_belongs_to_many :documents, join_table: :anthologies_documents
-	belongs_to :users
+	belongs_to :user
+	
+	def documents
+		ids = JSON.parse(self.toc)
+		# Per: http://stackoverflow.com/questions/1680627/activerecord-findarray-of-ids-preserving-order
+		Document.where(id: ids).index_by(&:id).values_at(*ids)
+	end
 end
