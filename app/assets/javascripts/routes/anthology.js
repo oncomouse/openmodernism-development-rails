08b3d0c7304e5b60d430d9e25470386c63ee1a09
onocmouse <onocmouse@gmail.com>
2015-08-11 14:05:02 -0500

    Initialize from documentList if it exists.

--- app/assets/javascripts/routes/anthology.js
+++ app/assets/javascripts/routes/anthology.js
@@ -31,7 +31,7 @@ define([
 			var current_documents = new DocumentCollection;
 			var document_list = (typeof app.documentList !== 'undefined') ? JSON.parse(currentAnthology.get('toc')) : currentAnthology.get('documents');
 			_.each(document_list, function(document) {
-				current_documents.add(new Document(document));
+				current_documents.add(typeof document === "number" ? app.documentList.get(document) : new Document(document));
 			});
 			currentAnthology.set('documents', current_documents);
 			React.render(React.createElement(AnthologyComponent, {model: currentAnthology}), $('#app').get(0));
